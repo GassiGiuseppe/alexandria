340b84f4d96052d39c4c0f209a99843e0caaf406
fSidel <f.sidella@studenti.poliba.it>
2021-12-09 11:31:38 +0100

    Implementazione ShelfViewModel per la comunicazione di repository e viewmodel

---
+++ app/src/main/java/com/swgroup/alexandria/data/database/ShelfViewModel.java
@@ -0,0 +1,53 @@
+package com.swgroup.alexandria.data.database;
+
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class ShelfViewModel extends AndroidViewModel {
+    private ShelfRepository repository;
+    private LiveData<List<ShelfEntry>> allEntries;
+
+    public ShelfViewModel(Application application) {
+        super(application);
+        repository = new ShelfRepository(application);
+        allEntries = repository.getAll();
+    }
+
+    public void insert(ShelfEntry entry) {
+        repository.insert(entry);
+    }
+
+    public void update(ShelfEntry entry) {
+        repository.update(entry);
+    }
+
+    public void delete(ShelfEntry entry) {
+        repository.delete(entry);
+    }
+
+    public LiveData<List<ShelfEntry>> getAllEntries() {
+        return allEntries;
+    }
+
+    public LiveData<List<ShelfEntry>> getByDataType(EntryType type) {return repository.getByDataType(type);}
+
+    public LiveData<List<ShelfEntry>> getByTitle(String title) {return repository.getByTitle(title);}
+
+    public LiveData<List<ShelfEntry>> getByAuthor(String author) {return repository.getByAuthor(author); }
+
+    public LiveData<List<ShelfEntry>> getByGenre(String genre) {return repository.getByGenre(genre);}
+
+    public LiveData<List<ShelfEntry>> getFavorites() {return repository.getFavorites();}
+
+    public LiveData<List<ShelfEntry>> searchByTitle(String title) {return repository.searchByTitle(title);}
+
+    public LiveData<List<ShelfEntry>> searchByAuthor(String author) {return  repository.searchByAuthor(author);}
+
+    public LiveData<List<ShelfEntry>> searchByGenre(String genre) {return repository.searchByGenre(genre);}
+
+}
+
