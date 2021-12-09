d6f66addfea6e55e338de7c207538cc2edc47db5
fSidel <f.sidella@studenti.poliba.it>
2021-12-09 14:14:34 +0100

    Test di popolazione del database (l'app non risulta avere errori di compilazione o esecuzione, ma purtroppo la recycleview non è ancora popolata. devo vedere come risolvere)

--- app/src/main/java/com/swgroup/alexandria/data/database/ShelfDatabase.java
+++ app/src/main/java/com/swgroup/alexandria/data/database/ShelfDatabase.java
@@ -1,10 +1,13 @@
 package com.swgroup.alexandria.data.database;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities = {ShelfEntry.class}, version = 1)
 public abstract class ShelfDatabase extends RoomDatabase {
@@ -33,8 +36,37 @@ public abstract class ShelfDatabase extends RoomDatabase {
         if (Instance == null) {
             Instance = Room
                     .databaseBuilder(context.getApplicationContext(), ShelfDatabase.class, NAME)
+                    .fallbackToDestructiveMigration()
+                    .addCallback(roomCallback)
                     .build();
         }
         return Instance;
+    }
+
+    // TODO: Eventualmente rimuovere questo metodo, serve solo per testare il database popolandolo con valaori predefiniti
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback(){
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateDBAsyncTask(Instance).execute();
+        }
+    };
+
+    private static class PopulateDBAsyncTask extends AsyncTask<Void, Void, Void> {
+        private ShelfDAO shelfDAO;
+
+        private PopulateDBAsyncTask(ShelfDatabase database) {
+            shelfDAO = database.shelfDao();
+        }
+        @Override
+        protected Void doInBackground(Void... voids) {
+            shelfDAO.insertEntry((new ShelfEntry("Neuromante", "Gibson", "Distopico")));
+            shelfDAO.insertEntry((new ShelfEntry("Blade Runner", "Dick", "Distopico")));
+            shelfDAO.insertEntry((new ShelfEntry("1984", "Orwell", "Distopico")));
+            shelfDAO.insertEntry((new ShelfEntry("Barone Rampante", "Calvino", "Romanzo")));
+            shelfDAO.insertEntry((new ShelfEntry("Nome della Rosa", "Eco", "Romanzo")));
+
+            return null;
+        }
     }
 }
