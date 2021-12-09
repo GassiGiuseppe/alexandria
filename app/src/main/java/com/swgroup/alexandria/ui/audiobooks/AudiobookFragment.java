88f3b5bdaf8f0c36aeaf6b35c26dcb5680ead721
fSidel <f.sidella@studenti.poliba.it>
2021-12-09 13:54:21 +0100

    Rimozione del vecchio ViewModel dedicato agli Audiobook, accomodando una versione più generica implementata in ShelfViewModel, utilizzata attualmente in AudiobookFragment

--- app/src/main/java/com/swgroup/alexandria/ui/audiobooks/AudiobookFragment.java
+++ app/src/main/java/com/swgroup/alexandria/ui/audiobooks/AudiobookFragment.java
@@ -11,30 +11,45 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.swgroup.alexandria.data.database.ShelfEntry;
 import com.swgroup.alexandria.databinding.FragmentAudiobookBinding;
+import com.swgroup.alexandria.ui.EntryAdapter;
+import com.swgroup.alexandria.ui.ShelfViewModel;
+
+import java.util.List;
 
 public class AudiobookFragment extends Fragment {
 
-    private AudiobookViewModel audiobookViewModel;
+    private ShelfViewModel shelfViewModel;
     private FragmentAudiobookBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        audiobookViewModel =
-                new ViewModelProvider(this).get(AudiobookViewModel.class);
 
-        binding = FragmentAudiobookBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        binding = FragmentAudiobookBinding
+                .inflate(inflater, container, false);
 
-        final TextView textView = binding.textAudiobook;
-        audiobookViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        RecyclerView recyclerView = binding.recyclerViewAudiobooks;
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.getActivity()));
+
+        EntryAdapter entryAdapter = new EntryAdapter();
+        recyclerView.setAdapter(entryAdapter);
+
+        shelfViewModel =
+                new ViewModelProvider(this).get(ShelfViewModel.class);
+        // TODO: Questa è solo una prova, dopo sostituisci per EntryType audiobook
+        shelfViewModel.getAllEntries().observe(getViewLifecycleOwner(), new Observer<List<ShelfEntry>>() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onChanged(List<ShelfEntry> shelfEntries) {
+                entryAdapter.setEntries(shelfEntries);
             }
         });
-        return root;
+
+        return binding.getRoot();
+
     }
 
     @Override
